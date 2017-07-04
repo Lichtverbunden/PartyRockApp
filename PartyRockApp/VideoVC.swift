20cf35fa33280bce763c688a00d0c6a94bafe5ef
Ken Krippeler <kriken19@gmail.com>
2017-07-04 11:28:47 +0200

    Finishing Up the UI

---
+++ PartyRockApp/VideoVC.swift
@@ -0,0 +1,23 @@
+//
+//  VideoVC.swift
+//  PartyRockApp
+//
+//  Created by Ken Krippeler on 04.07.17.
+//  Copyright © 2017 Lichtverbunden. All rights reserved.
+//
+
+import UIKit
+
+class VideoVC: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+    
+    }
+
+
+
+}
